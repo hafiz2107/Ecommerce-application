7ec67cd378b9a9bdfca6de2a4b5aebfaa5e8e109
hafiz2107 <hafizshafi001@gmail.com>
2021-10-03 21:17:50 +0530

    cart completed 2

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -7,56 +7,55 @@ var objectId = require('mongodb').ObjectId
 module.exports = {
 
     // User signup data uploading
-    userSignup : (userData)=>{
-        return new Promise( async(resolve,reject)=>{
-            var allDatas = await db.get().collection(collection.userDatabase).findOne({username:userData.username})
+    userSignup: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            var allDatas = await db.get().collection(collection.userDatabase).findOne({ username: userData.username })
 
-            if(allDatas == null){
+            if (allDatas == null) {
                 allDatas = 'm'
             }
 
-            if (allDatas.username == userData.username)
-            {
+            if (allDatas.username == userData.username) {
                 resolve(false)
             }
-            else{
+            else {
                 userData.pwd = await bcrypt.hash(userData.pwd, 10)
                 userData.pwdc = await bcrypt.hash(userData.pwdc, 10)
-                var details =await db.get().collection(collection.userDatabase).insertOne(userData)
+                var details = await db.get().collection(collection.userDatabase).insertOne(userData)
                 console.log("details is :", details);
-                var data =await db.get().collection(collection.userDatabase).findOne({ _id: details.insertedId })
+                var data = await db.get().collection(collection.userDatabase).findOne({ _id: details.insertedId })
                 resolve(data);
             }
         })
-        
-    },
-// Checking Whwen logging in
-    doLogin :(userData)=>{
 
-        return new Promise(async(resolve,reject)=>{
+    },
+    // Checking Whwen logging in
+    doLogin: (userData) => {
+
+        return new Promise(async (resolve, reject) => {
             let loginStatus = false
             let response = {};
-            var userDetails = await db.get().collection(collection.userDatabase).findOne({username:userData.username});
-            console.log("User details : ",userDetails)
-            
-            if(userDetails){
+            var userDetails = await db.get().collection(collection.userDatabase).findOne({ username: userData.username });
+            console.log("User details : ", userDetails)
+
+            if (userDetails) {
                 console.log("Comparing", userDetails.pwd, "And", userData.pwd)
-                bcrypt.compare(userData.pwd, userDetails.pwd).then((status)=>{
-                    console.log("The status is : ",status);
-                    if(status){
+                bcrypt.compare(userData.pwd, userDetails.pwd).then((status) => {
+                    console.log("The status is : ", status);
+                    if (status) {
                         response.user = userDetails;
                         response.status = true
                         loginStatus = true;
-                        console.log("response : ",response);
+                        console.log("response : ", response);
                         resolve(response)
                     }
-                    else{
+                    else {
                         console.log("Login Error");
                         response.passError = true;
                         resolve(response)
                     }
                 })
-            }else{
+            } else {
                 console.log("Login Failed");
                 response.emailError = true
                 resolve(response)
@@ -64,166 +63,166 @@ module.exports = {
         })
     },
     // To get all products from database
-    getAllProducts : ()=>{
-        return new Promise(async(resolve, reject)=>{
+    getAllProducts: () => {
+        return new Promise(async (resolve, reject) => {
             var allProducts = await db.get().collection(collection.newproducts).find().toArray();
             resolve(allProducts)
         })
     },
     // TO get a single peoduct form database
-    singleProduct : (productId)=>{
-        return new Promise(async(resolve, reject)=>{
-            var product = await db.get().collection(collection.newproducts).findOne({_id:objectId(productId)})
+    singleProduct: (productId) => {
+        return new Promise(async (resolve, reject) => {
+            var product = await db.get().collection(collection.newproducts).findOne({ _id: objectId(productId) })
 
-            console.log("THe categpry is : ",product.category);
-            var relatedProduct = await db.get().collection(collection.newproducts).find({category:product.category}).limit(4).toArray()
-            resolve([product , relatedProduct])
+            console.log("THe categpry is : ", product.category);
+            var relatedProduct = await db.get().collection(collection.newproducts).find({ category: product.category }).limit(4).toArray()
+            resolve([product, relatedProduct])
         })
     },
     // To check the mobile number upon entering for OTP 
-    checkMobNo : (data)=>{
-        console.log("The mobile number is : ",data)
-        return new Promise(async(resolve,reject)=>{
+    checkMobNo: (data) => {
+        console.log("The mobile number is : ", data)
+        return new Promise(async (resolve, reject) => {
             var mobile = data.mobileno
             var country = data.countryCode
-           var user = data.user
-            var user = await db.get().collection(collection.userDatabase).findOne({mobile : mobile},{countryCode : country})
+            var user = data.user
+            var user = await db.get().collection(collection.userDatabase).findOne({ mobile: mobile }, { countryCode: country })
             console.log(user);
-            if(user){
+            if (user) {
                 resolve(user)
-            }else{
+            } else {
                 resolve(false)
             }
         })
     },
     // TO update the password When clicking  the forgotpassword
-    updatePassword :  (body , user)=>{
+    updatePassword: (body, user) => {
         return new Promise(async (resolve, reject) => {
             body.newpass = await bcrypt.hash(body.newpass, 10)
-            data = await db.get().collection(collection.userDatabase).updateOne({_id : objectId(user._id)},{$set:{pwd : body.newpass}})
-            console.log("Data after setting password : ",data)
-            if(data){
+            data = await db.get().collection(collection.userDatabase).updateOne({ _id: objectId(user._id) }, { $set: { pwd: body.newpass } })
+            console.log("Data after setting password : ", data)
+            if (data) {
                 resolve(data)
-            }else{
+            } else {
                 resolve(false)
             }
         })
     },
-    addToCart : (proId , userId) =>{
+    addToCart: (proId, userId) => {
         let proObj = {
-            item : objectId(proId),
-            quantity : 1
+            item: objectId(proId),
+            quantity: 1
         }
-        return new Promise(async(resolve,reject)=>{
-            let userCart =  await db.get().collection(collection.cartItems).findOne({user : objectId(userId)})
+        return new Promise(async (resolve, reject) => {
+            let userCart = await db.get().collection(collection.cartItems).findOne({ user: objectId(userId) })
 
-            if(userCart){
+            if (userCart) {
                 let proExist = userCart.products.findIndex(product => product.item == proId)
-                console.log("indec : ",proExist)
+                console.log("indec : ", proExist)
 
-                if(proExist != -1){
-                    db.get().collection(collection.cartItems).updateOne({user : objectId(userId) ,'products.item': objectId(proId) }, { $inc: { 'products.$.quantity': 1 } }).then(() => {
-                         resolve() 
+                if (proExist != -1) {
+                    db.get().collection(collection.cartItems).updateOne({ user: objectId(userId), 'products.item': objectId(proId) }, { $inc: { 'products.$.quantity': 1 } }).then(() => {
+                        resolve()
                     })
                 }
-                else{
+                else {
                     db.get().collection(collection.cartItems).updateOne({ user: objectId(userId) }, { $push: { products: proObj } }).then((response) => {
                         resolve();
                     })
                 }
-                  
+
             }
-            else{
+            else {
                 let cartObj = {
-                    user : objectId(userId),
-                    products : [proObj]
+                    user: objectId(userId),
+                    products: [proObj]
                 }
-                db.get().collection(collection.cartItems).insertOne(cartObj).then((response)=>{
+                db.get().collection(collection.cartItems).insertOne(cartObj).then((response) => {
                     resolve(response)
                 })
-                }
+            }
         })
     },
-    getCartProducts : (userId) =>{
-        return new Promise(async(resolve,reject)=>{
-           
+    getCartProducts: (userId) => {
+        return new Promise(async (resolve, reject) => {
+
             // Doing aggregation
             let cartItems = await db.get().collection(collection.cartItems).aggregate([
                 {
                     // Matching the element in the user field in the colleciton cartitems and anf taking that field
-                    $match : {user : objectId(userId)}
+                    $match: { user: objectId(userId) }
                 },
                 {
-                    $unwind:'$products'
-                },{
-                    $project:{
-                        item : '$products.item',
-                        quantity : '$products.quantity'
+                    $unwind: '$products'
+                }, {
+                    $project: {
+                        item: '$products.item',
+                        quantity: '$products.quantity'
                     }
-                },{
+                }, {
                     $lookup: {
-                        from : collection.newproducts,
-                        localField : 'item',
-                        foreignField : '_id',
-                        as : 'product'
+                        from: collection.newproducts,
+                        localField: 'item',
+                        foreignField: '_id',
+                        as: 'product'
                     }
-                },{
-                    $project : {
-                        item : 1,
-                        quantity : 1,
-                        product : {$arrayElemAt : ['$product',0]}
+                }, {
+                    $project: {
+                        item: 1,
+                        quantity: 1,
+                        product: { $arrayElemAt: ['$product', 0] }
                     }
                 }
-                   
-                
-                
+
+
+
             ]).toArray()
             console.log(cartItems);
-            if(cartItems[0]){
+            if (cartItems[0]) {
                 resolve(cartItems)
-            }else{
+            } else {
                 resolve(false)
             }
-            
+
         })
     },
-    getCartCount : (userId) =>{
-        return new Promise(async(resolve,reject)=>{
+    getCartCount: (userId) => {
+        return new Promise(async (resolve, reject) => {
             let count = 0
-            let cart = await db.get().collection(collection.cartItems).findOne({user : objectId(userId)})
-             if(cart){
+            let cart = await db.get().collection(collection.cartItems).findOne({ user: objectId(userId) })
+            if (cart) {
                 count = cart.products.length
-             }
-             resolve(count)
+            }
+            resolve(count)
         })
     },
-    changeProductQuantity : (details)=>{
-        console.log("Teh detaisl are : ",details)
+    changeProductQuantity: (details) => {
+        console.log("Teh detaisl are : ", details)
         details.count = parseInt(details.count)
         details.quantity = parseInt(details.quantity)
-       
 
-        return new Promise((resolve,reject)=>{
-           
-            if(details.count == -1 && details.quantity == 1){
-                db.get().collection(collection.cartItems).updateOne({_id : objectId(details.cart)},
-                {
-                    $pull :{products:{item:objectId(details.product_id)}}
-                }
-                ).then((response)=>{
-                    resolve({removeProduct : true})
+
+        return new Promise((resolve, reject) => {
+
+            if (details.count == -1 && details.quantity == 1) {
+                db.get().collection(collection.cartItems).updateOne({ _id: objectId(details.cart) },
+                    {
+                        $pull: { products: { item: objectId(details.product_id) } }
+                    }
+                ).then((response) => {
+                    resolve({ removeProduct: true })
                 })
-            }else{
-                console.log("The details are :  ",details);
-                db.get().collection(collection.cartItems).updateOne({ _id: objectId(details.cart), 'products.item': objectId(details.product_id)},
-                {
-                    $inc:{'products.$.quantity':details.count}
-                }
-                ).then((response)=>{
+            } else {
+                console.log("The details are :  ", details);
+                db.get().collection(collection.cartItems).updateOne({ _id: objectId(details.cart), 'products.item': objectId(details.product_id) },
+                    {
+                        $inc: { 'products.$.quantity': details.count }
+                    }
+                ).then((response) => {
                     resolve(true)
                 })
             }
         })
     }
-    
-} 
+
+}
--- routes/user.js
+++ routes/user.js
@@ -10,56 +10,56 @@ var userToresetPass
 var currentUser
 
 /* GET home page. */
-router.get('/',async function(req, res, next) {
-  if(req.session.LoggedIn){
+router.get('/', async function (req, res, next) {
+  if (req.session.LoggedIn) {
     console.log("1.Home page loaded at if");
-    
+
     currentUser = req.session.user
     logStatus = req.session.LoggedIn
     userId = req.session.userDetails
     // Calling function to get the cart count
-    let cartCount =await userHelpers.getCartCount(req.session.userDetails)
+    let cartCount = await userHelpers.getCartCount(req.session.userDetails)
 
 
     userHelpers.getAllProducts().then((products) => {
       console.log("inside : get all products");
-      res.render('user/user-home', { title: 'Home', user: true, currentUser, typeOfPersonUser: true, products,logStatus,cartCount});
+      res.render('user/user-home', { title: 'Home', user: true, currentUser, typeOfPersonUser: true, products, logStatus, cartCount });
     })
-    
-  } else if (req.session.LoggedInThruOtp){
+
+  } else if (req.session.LoggedInThruOtp) {
     console.log("1.Home page loaded at else if");
     req.session.LoggedIn = true
     userHelpers.getAllProducts().then((products) => {
       console.log("inside : get all products");
       currentUser = req.session.resetUser
-      
+
       res.render('user/user-home', { title: 'Home', user: true, currentUser, typeOfPersonUser: true, products });
     })
   }
-  else{
+  else {
     console.log("1.1Home page loaded at else");
     var currentUser = req.session.user
     userHelpers.getAllProducts().then((products) => {
       console.log("inside : get all products");
-      res.render('user/user-home', { title: 'Home', user: true, currentUser: false, typeOfPersonUser: true,products });
+      res.render('user/user-home', { title: 'Home', user: true, currentUser: false, typeOfPersonUser: true, products });
     })
-    
+
   }
 
 });
 
 // Getting login page
-router.get('/login',(req,res)=>{
-  if(req.session.LoggedIn){
+router.get('/login', (req, res) => {
+  if (req.session.LoggedIn) {
     res.redirect('/')
   }
-  else{
+  else {
     console.log("1.1 Login Page loaded");
     loggedInErr = req.session.loggedInErr
     emailError = req.session.emailError;
     passError = req.session.passError;
-    
-    res.render('user/user-login', { title: 'Login', loginAndSignup: true, loggedInErr, typeOfPersonUser: true , emailError, passError})
+
+    res.render('user/user-login', { title: 'Login', loginAndSignup: true, loggedInErr, typeOfPersonUser: true, emailError, passError })
     emailError = false
     req.session.emailError = false;
     passError = false
@@ -71,23 +71,23 @@ router.get('/login',(req,res)=>{
 })
 
 // Posting Logged in user's data
-router.post('/login',(req,res)=>{
+router.post('/login', (req, res) => {
   console.log("1.2 Posting logged in users data");
-  userHelpers.doLogin(req.body).then((response)=>{
-        
-    if(response.status){
-      
+  userHelpers.doLogin(req.body).then((response) => {
+
+    if (response.status) {
+
       req.session.userDetails = response.user._id;
       req.session.user = req.body;
       var userSession = req.session.user;
       req.session.LoggedIn = true
       res.redirect('/')
-    }else if(response.passError){
+    } else if (response.passError) {
       req.session.loggedInErr = true
       req.session.passError = response.passError
       console.log("At else in the login post");
       res.redirect('/login');
-    }else if(response.emailError){
+    } else if (response.emailError) {
       req.session.loggedInErr = true
       req.session.emailError = response.emailError
 
@@ -96,16 +96,16 @@ router.post('/login',(req,res)=>{
     }
   })
 })
- 
+
 // Gettting sign up page
-router.get('/signup',(req,res)=>{
-  if(req.session.LoggedIn){
+router.get('/signup', (req, res) => {
+  if (req.session.LoggedIn) {
     res.redirect('/')
   }
-  else{
+  else {
     userNameAlreadyExist = req.session.userNameAlreadyExist
     console.log("1.2 Getting signup page");
-    res.render('user/user-signup', { title: 'Signup', loginAndSignup: true, userNameAlreadyExist, typeOfPersonUser: true})
+    res.render('user/user-signup', { title: 'Signup', loginAndSignup: true, userNameAlreadyExist, typeOfPersonUser: true })
     userNameAlreadyExist = false;
     req.session.userNameAlreadyExist = false;
   }
@@ -113,14 +113,14 @@ router.get('/signup',(req,res)=>{
 })
 
 // posting Signed up data
-router.post('/signup',(req,res)=>{
+router.post('/signup', (req, res) => {
   console.log("1.3 Posting Signedup user data");
   console.log(req.body);
-  userHelpers.userSignup(req.body).then((response)=>{
-    console.log("the response is : ",response);
-    if(response){
+  userHelpers.userSignup(req.body).then((response) => {
+    console.log("the response is : ", response);
+    if (response) {
       res.redirect('/login')
-    }else{
+    } else {
       req.session.userNameAlreadyExist = true;
       res.redirect('/signup')
     }
@@ -130,35 +130,35 @@ router.post('/signup',(req,res)=>{
 })
 
 // Getting Forgot password PAge
-router.get('/forgotpassword',(req,res)=>{
+router.get('/forgotpassword', (req, res) => {
   console.log(req.body)
   res.render('user/user-forgotpassword', { title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true })
 })
 
 // Posting Forgot password PAge and checking the user exist or not 
-router.post('/forgotpassword',(req,res)=>{
+router.post('/forgotpassword', (req, res) => {
   mobileError = false
-  mobile = parseInt(''+req.body.countryCode + req.body.mobileno)
-  
-  userHelpers.checkMobNo(req.body).then((user)=>{
+  mobile = parseInt('' + req.body.countryCode + req.body.mobileno)
+
+  userHelpers.checkMobNo(req.body).then((user) => {
     userToresetPass = user;
-    if(user){
+    if (user) {
       // If response is true sending the OTP Message
-        client.verify.services(keys.serviceid)
+      client.verify.services(keys.serviceid)
         .verifications
-        .create({ to: '+'+mobile, channel: 'sms' }).then((data)=>{
+        .create({ to: '+' + mobile, channel: 'sms' }).then((data) => {
           // Data will  be recieved with The send status adn all
-          res.render('user/user-otp', { title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true , mobile })
-         
-      }).catch((err)=>{
-        // If there is any error THe actch block will catch it
-        res.redirect('/404')
-        console.log("The error in sending message : ",err);
-      })
-    }else{
+          res.render('user/user-otp', { title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true, mobile })
+
+        }).catch((err) => {
+          // If there is any error THe actch block will catch it
+          res.redirect('/404')
+          console.log("The error in sending message : ", err);
+        })
+    } else {
       // Mobile number entered is wrong
       mobileError = true
-      res.render('user/user-forgotpassword',{ title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true ,mobileError})
+      res.render('user/user-forgotpassword', { title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true, mobileError })
       mobileError = false
     }
   })
@@ -166,46 +166,46 @@ router.post('/forgotpassword',(req,res)=>{
 
 
 // Posting The verified OTP adn redirecting to home page
-router.post('/otpverify',(req,res)=>{
+router.post('/otpverify', (req, res) => {
   var user = req.body
   // userHelpers.checkMobNo(req.body)
   // Checking whether the Entered OTP Is wrong
   client.verify
     .services(keys.serviceid)
-    .verificationChecks.create({ to: '+'+req.body.phone, code: req.body.otp })
+    .verificationChecks.create({ to: '+' + req.body.phone, code: req.body.otp })
     .then((verification_check) => {
       // If the OTP is wright It will give status as Approved else it will give status as Pending
-      if (verification_check.status  == 'approved') {
+      if (verification_check.status == 'approved') {
         console.log("RESET : ", userToresetPass)
-        res.render('user/user-resetpassword', { title: 'Verify OTP', loginAndSignup: true, typeOfPersonUser: true})
-      }else{
+        res.render('user/user-resetpassword', { title: 'Verify OTP', loginAndSignup: true, typeOfPersonUser: true })
+      } else {
         mobile = req.body.phone
         otpError = true
-        res.render('user/user-otp', { title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true, mobile ,otpError})
+        res.render('user/user-otp', { title: 'Forgot Password', loginAndSignup: true, typeOfPersonUser: true, mobile, otpError })
         otpError = false
       }
-    }).catch((err)=>{
+    }).catch((err) => {
       console.log(err);
     })
 })
 
 // Posting reseting password that user entered
-router.post('/resetpassword',(req,res)=>{
+router.post('/resetpassword', (req, res) => {
   console.log("user ro : ", userToresetPass);
-  console.log("Patch : ",req.body);
+  console.log("Patch : ", req.body);
 
-  userHelpers.updatePassword(req.body,userToresetPass).then((result)=>{
-    if(result){
+  userHelpers.updatePassword(req.body, userToresetPass).then((result) => {
+    if (result) {
       currentUser = userToresetPass
       resetSuccess = true
       req.session.LoggedIn = false
       req.session.LoggedInThruOtp = true
       req.session.resetUser = currentUser
       res.redirect('/')
-      
-    }else{
+
+    } else {
       newPassError = true
-      res.render('user/user-user-resetpassword', { title: 'Reset password', loginAndSignup: true, typeOfPersonUser: true , newPassError})
+      res.render('user/user-user-resetpassword', { title: 'Reset password', loginAndSignup: true, typeOfPersonUser: true, newPassError })
       newPassError = false
     }
   })
@@ -215,53 +215,53 @@ router.post('/resetpassword',(req,res)=>{
 router.get('/productview/:id', (req, res) => {
   console.log("Product single view page loaded");
   var proId = req.params.id
-  userHelpers.singleProduct(proId).then(([singleProduct , relatedProduct])=>{
-    res.render('user/user-singleproduct', { title: 'Product', user: true, typeOfPersonUser: true, singleProduct, relatedProduct})
+  userHelpers.singleProduct(proId).then(([singleProduct, relatedProduct]) => {
+    res.render('user/user-singleproduct', { title: 'Product', user: true, typeOfPersonUser: true, singleProduct, relatedProduct })
   })
 })
 
 // Gettign the cart page
-router.get('/cart',async(req,res)=>{
+router.get('/cart', async (req, res) => {
   user = req.session.userDetails
   logStatus = req.session.LoggedIn
   currentUser = req.session.user
- 
-  if (req.session.LoggedIn){
-    let products = await userHelpers.getCartProducts(req.session.userDetails)
-      if (products) {
-        res.render('user/user-cart', { title: 'Product', products, user: true, typeOfPersonUser: true, currentUser})
-      }
-      else {
-        res.render('user/user-cart', { title: 'Product', currentUser, user: true, typeOfPersonUser: true,logStatus})
-      }
 
-  }else{
+  if (req.session.LoggedIn) {
+    let products = await userHelpers.getCartProducts(req.session.userDetails)
+    if (products) {
+      res.render('user/user-cart', { title: 'Product', products, user: true, typeOfPersonUser: true, currentUser })
+    }
+    else {
+      res.render('user/user-cart', { title: 'Product', currentUser, user: true, typeOfPersonUser: true, logStatus })
+    }
+
+  } else {
     res.redirect('/login')
   }
-  
- 
-  
+
+
+
 })
 
 // Adding items to the cart
-router.get('/add-to-cart/:id',(req,res)=>{
+router.get('/add-to-cart/:id', (req, res) => {
   console.log("apoi call")
-      user = req.session.userDetails
+  user = req.session.userDetails
   // console.log("IDDD : ",user);
-    userHelpers.addToCart(req.params.id,req.session.userDetails).then(()=>{
-    res.json({status:true})
+  userHelpers.addToCart(req.params.id, req.session.userDetails).then(() => {
+    res.json({ status: true })
   })
 })
 
-router.post('/change-product-quantity',(req,res)=>{
-  console.log("SAISoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo : ",req.body)
-  userHelpers.changeProductQuantity(req.body).then((response)=>{
+router.post('/change-product-quantity', (req, res) => {
+  console.log("SAISoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo : ", req.body)
+  userHelpers.changeProductQuantity(req.body).then((response) => {
     res.json(response)
   })
 })
 
 // Logout
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
   console.log("Loggin out");
   req.session.LoggedIn = false
   req.session.LoggedInThruOtp = false
@@ -270,7 +270,7 @@ router.get('/logout',(req,res)=>{
 })
 
 
-router.get('/404',(req, res)=>{
+router.get('/404', (req, res) => {
   res.render('404', { title: 'Error 404', loginAndSignup: true, typeOfPersonUser: true })
 })
 module.exports = router;
